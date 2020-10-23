d239a0579eb9da919f5faaef4154326eeef046bd
MartinBoylan <63868146+MartinBoylan@users.noreply.github.com>
2020-10-23 18:20:40 +0100

    about as much as I can do

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//You can edit ALL of the code here
 let input = document.createElement("input");
 let header = document.createElement("header");
+let nav = document.querySelector("nav");
+console.log(nav);
 input.type = "text";
 input.id = "userSearch";
 let display = document.createElement("p");
@@ -10,11 +11,13 @@ header.appendChild(input);
 header.appendChild(display);
 let searchInput = document.getElementById("userSearch");
 let allEpisodes = getAllEpisodes();
+
+let root = document.querySelector("#root");
+
 // fetch episodes from API
 function fetchEpisodes(showId = 82) {
-    let proxyUrl = `https://cors-anywhere.herokuapp.com/`;
-    targetUrl = `https://api.tvmaze.com/shows/${showId}/episodes`;
-    fetch(proxyUrl + targetUrl)
+    let targetUrl = `https://api.tvmaze.com/shows/${showId}/episodes`;
+    fetch(targetUrl)
         .then((response) => response.json())
         .then((data) => {
             allEpisodes = data;
@@ -27,12 +30,17 @@ function fetchEpisodes(showId = 82) {
 
 // populate page with elements made from the allEpisodes array
 function makePageForEpisodes(episodeList) {
-    const rootElem = document.getElementById("root");
+    let rootElem = document.getElementById("root");
     let container = document.createElement("section");
+
     rootElem.innerHTML = "";
 
     display.textContent = `${episodeList.length} matches`;
-
+    nav.addEventListener("click", () => {
+        setup();
+        let section = document.querySelector("section");
+        section.style.display = "none";
+    });
     episodeList.forEach(function (episode) {
         let episodeDiv = document.createElement("div");
         episodeDiv.classList = "episodeStyle";
@@ -122,4 +130,72 @@ function selectShow(event) {
     fetchEpisodes(showId);
 }
 
-fetchEpisodes();
+//fetchEpisodes();
+
+function setup() {
+    let doItAgain = getAllShows();
+    ///body.textContent = "";
+    doItAgain.forEach((show) => {
+        let showDiv = document.createElement("div");
+        showDiv.className = "showDiv";
+
+        root.appendChild(showDiv);
+        let showHeading = document.createElement("h1");
+        showHeading.setAttribute("showId", show.id);
+        showHeading.textContent = `${show.name}`;
+        let image = document.createElement("img");
+
+        if (show.image === null) {
+            image.src =
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
+        } else {
+            image.src = `${show.image.medium}`;
+        }
+
+        let rest = document.createElement("div");
+
+        rest.textContent = `Status: ${show.status}; Genres: ${show.genres}; Rating: ${show.rating.average}; Runtime: ${show.runtime}`;
+
+        body.appendChild(showDiv);
+        showDiv.appendChild(showHeading);
+        showHeading.insertAdjacentElement("afterend", image);
+        image.insertAdjacentElement("afterend", rest);
+        rest.insertAdjacentHTML("afterend", show.summary);
+        showHeading.addEventListener("click", function (e) {
+            //console.log(e.target.textContent);
+            let particularShows = getAllShows();
+            //let divSiblings = getSiblings(showDiv);
+            let divSiblings = document.querySelectorAll(".showDiv");
+            console.log("e.target.parentElement: ", e.target.parentElement);
+            e.target.parentElement.innerHTML = "";
+
+            divSiblings.forEach((eachDiv) => {
+                console.log("divSiblings: ", eachDiv);
+                eachDiv.style.display = "none";
+            });
+
+            let showId = e.target.getAttribute("showId");
+            console.log("target", e.currentTarget);
+            console.log("Show ID", showId);
+            fetchEpisodes(showId);
+        });
+        //heading.addEventListener(onclick, fetchEpisodes)
+    });
+}
+window.onload = setup;
+
+let getSiblings = function (elem) {
+    // Setup siblings array and get the first sibling
+    let siblings = [];
+    let sibling = elem.parentNode.firstChild;
+
+    // Loop through each sibling and push to the array
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== elem) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+
+    return siblings;
+};
